5580a1c7f87fb6db5eccd91c704f9ad082202e2a
hserranome <hendy@hserrano.me>
2024-04-19 00:02:58 +0200

    use crypto for register

--- app/auth/signup/email.tsx
+++ app/auth/signup/email.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import * as Crypto from "expo-crypto";
 
 import { Text, View } from "@/components/elements/Themed";
 import { CTALinkButton } from "@/components/elements/CTALinkButton";
@@ -9,6 +10,34 @@ import { AppwriteException } from "react-native-appwrite/src";
 import { Input } from "@/components/elements/Input";
 import { Button } from "@/components/elements/Button";
 import { ErrorMessage } from "@/components/elements/ErrorMessage";
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+	content: {
+		flex: 1,
+		justifyContent: "flex-end",
+		width: "90%",
+		marginBottom: 40,
+	},
+	title: {
+		textAlign: "center",
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 20,
+	},
+	form: {
+		marginBottom: 20,
+	},
+	inputContainer: {
+		marginBottom: 10,
+	},
+	button: {
+		marginTop: 10,
+	},
+});
 
 const login = async (email: string, password: string) => {
 	await appw.account.createEmailSession(email, password);
@@ -33,7 +62,7 @@ export default function SignUpEmailScreen() {
 
 	const onSubmit = useCallback(async ({ email, password, username }: SignUpFormData) => {
 		try {
-			await appw.account.create("blabla", email, password, username);
+			await appw.account.create(Crypto.randomUUID(), email, password, username);
 			await login(email, password);
 		} catch (error: AppwriteException | any) {
 			if (error instanceof AppwriteException) {
@@ -43,8 +72,6 @@ export default function SignUpEmailScreen() {
 						switch (error.code) {
 							case 409:
 								return "Email is already in use";
-							case 429:
-								return "Too many requests, please try again later";
 							default:
 								console.error(error, error.code);
 								return `An unexpected error occurred (${error.code})`;
@@ -56,85 +83,67 @@ export default function SignUpEmailScreen() {
 		}
 	}, []);
 
-	console.log(errors);
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Sign Up</Text>
-			<View style={styles.form}>
-				<Input
-					control={control}
-					name="email"
-					placeholder="Email"
-					rules={{
-						required: {
-							value: true,
-							message: "Email is required",
-						},
-						pattern: {
-							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-							message: "invalid email address",
-						},
-					}}
-					errors={errors}
-					disabled={isSubmitting}
-					returnKeyType="next"
-					onSubmitEditing={() => setFocus("password")}
-					containerStyle={styles.inputContainer}
-				/>
-				<Input
-					control={control}
-					name="password"
-					placeholder="Password"
-					rules={{
-						required: { value: true, message: "Password is required" },
-						minLength: { value: 6, message: "Password must be at least 6 characters" },
-					}}
-					errors={errors}
-					disabled={isSubmitting}
-					returnKeyType="next"
-					onSubmitEditing={() => setFocus("username")}
-					containerStyle={styles.inputContainer}
-				/>
-				<Input
-					control={control}
-					name="username"
-					placeholder="Username"
-					rules={{
-						required: { value: true, message: "Username is required" },
-						minLength: { value: 5, message: "Username must be at least 5 characters" },
-					}}
-					errors={errors}
-					disabled={isSubmitting}
-					onSubmitEditing={handleSubmit(onSubmit)}
-					containerStyle={styles.inputContainer}
-				/>
-				<Button disabled={isSubmitting} onPress={handleSubmit(onSubmit)} />
-				<ErrorMessage message={errors?.root?.serverError?.message} />
+			<View style={styles.content}>
+				<Text style={styles.title}>Sign Up</Text>
+				<View style={styles.form}>
+					<Input
+						control={control}
+						name="email"
+						placeholder="Email"
+						rules={{
+							required: {
+								value: true,
+								message: "Email is required",
+							},
+							pattern: {
+								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+								message: "invalid email address",
+							},
+						}}
+						errors={errors}
+						disabled={isSubmitting}
+						returnKeyType="next"
+						onSubmitEditing={() => setFocus("password")}
+						containerStyle={styles.inputContainer}
+					/>
+					<Input
+						control={control}
+						name="password"
+						placeholder="Password"
+						rules={{
+							required: { value: true, message: "Password is required" },
+							minLength: { value: 6, message: "Password must be at least 6 characters" },
+						}}
+						errors={errors}
+						disabled={isSubmitting}
+						returnKeyType="next"
+						onSubmitEditing={() => setFocus("username")}
+						containerStyle={styles.inputContainer}
+					/>
+					<Input
+						control={control}
+						name="username"
+						placeholder="Username"
+						rules={{
+							required: { value: true, message: "Username is required" },
+							minLength: { value: 5, message: "Username must be at least 5 characters" },
+						}}
+						errors={errors}
+						disabled={isSubmitting}
+						onSubmitEditing={handleSubmit(onSubmit)}
+						containerStyle={styles.inputContainer}
+					/>
+					<Button
+						label="Sign up"
+						disabled={isSubmitting}
+						onPress={handleSubmit(onSubmit)}
+						containerStyle={styles.button}
+					/>
+					<ErrorMessage message={errors?.root?.serverError?.message} />
+				</View>
 			</View>
-			<CTALinkButton href="../auth/signup/email" label="Sign up with email" />
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
-	form: {
-		width: "80%",
-	},
-	inputContainer: {
-		marginBottom: 10,
-	},
-});
