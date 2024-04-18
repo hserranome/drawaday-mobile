eb9343f5c4062cbb264472e4b36b4b01c35b28e1
hserranome <hendy@hserrano.me>
2024-04-19 00:02:47 +0200

    button labels and styles

--- components/elements/Button/Button.tsx
+++ components/elements/Button/Button.tsx
@@ -1,16 +1,33 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native";
 
 const styles = StyleSheet.create({
-	submitButton: {
-		backgroundColor: "blue",
-		padding: 10,
-		borderRadius: 5,
+	button: {
+		width: "100%",
+		backgroundColor: "#f0f0f0",
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 1000,
+		overflow: "hidden",
+	},
+	text: {
+		color: "#000",
+		width: "100%",
+		textAlign: "center",
+		fontSize: 20,
+		fontWeight: "600",
 	},
 });
 
-export type ButtonProps = { onPress?: () => void; disabled?: boolean };
-export const Button = ({ onPress, disabled }: ButtonProps) => (
-	<Pressable style={styles.submitButton} onPress={onPress} disabled={disabled}>
-		<Text>Sign up</Text>
-	</Pressable>
+export type ButtonProps = {
+	label?: string;
+	onPress?: () => void;
+	disabled?: boolean;
+	containerStyle?: StyleProp<ViewStyle>;
+};
+export const Button = ({ label, onPress, disabled, containerStyle }: ButtonProps) => (
+	<View style={containerStyle}>
+		<Pressable style={styles.button} onPress={onPress} disabled={disabled}>
+			<Text style={styles.text}>{label}</Text>
+		</Pressable>
+	</View>
 );
