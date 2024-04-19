5a772cc3f1d3c810215750b69c728dd23d3fd5a3
hserranome <hendy@hserrano.me>
2024-04-19 02:19:25 +0200

    AppwriteProvider init

--- components/providers/AppwriteProvider.tsx
+++ components/providers/AppwriteProvider.tsx
@@ -1,44 +1,65 @@
 import { appw } from "@/api/appwrite";
 import { SplashScreen, useRouter } from "expo-router";
 import { PropsWithChildren, createContext, useEffect, useState } from "react";
-import { Models } from "react-native-appwrite/src";
+import { Account, Client, Models } from "react-native-appwrite/src";
 
 type AppwriteContextType = {
-	initializing: boolean;
+	initialized: boolean;
 	user: Models.User<Models.Preferences> | null;
-	setUser: (user: Models.User<Models.Preferences> | null) => void;
+	login: (email: string, password: string) => Promise<void>;
+	logout: () => Promise<void>;
 };
 
 export const AppwriteContext = createContext<AppwriteContextType>({} as AppwriteContextType);
 
 export function AppwriteProvider({ children }: PropsWithChildren) {
-	const [initializing, setInitializing] = useState(true);
+	const [initialized, setInitialized] = useState(false);
 	const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
+
 	const router = useRouter();
 
+	async function init() {
+		try {
+			const loggedIn = await appw.account.get();
+			setUser(loggedIn);
+		} catch (err) {
+			setUser(null);
+		}
+		setInitialized(true);
+	}
+
 	useEffect(() => {
-		(async () => {
-			const loggedUser = await appw.account.get();
-			console.log("loggedUser", loggedUser);
-			SplashScreen.hideAsync();
-			if (!loggedUser) {
-				router.replace("/");
-				return;
-			}
-			setUser(loggedUser);
-			setInitializing(false);
-		})();
+		init();
 	}, []);
 
-	return (
-		<AppwriteContext.Provider
-			value={{
-				initializing,
-				user,
-				setUser,
-			}}
-		>
-			{children}
-		</AppwriteContext.Provider>
-	);
+	useEffect(() => {
+		if (initialized) {
+			if (!user) router.replace("/auth/");
+			SplashScreen.hideAsync();
+		}
+	}, [initialized]);
+
+	const login = async (email: string, password: string) => {
+		try {
+			await appw.account.createEmailSession(email, password);
+			setUser(await appw.account.get());
+			router.replace("/(app)/");
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const logout = async () => {
+		try {
+			await appw.account.deleteSession("current");
+			setUser(null);
+			router.replace("/auth/");
+		} catch (error) {
+			console.error(JSON.stringify(error));
+		}
+	};
+
+	useEffect(() => {}, [user]);
+
+	return <AppwriteContext.Provider value={{ initialized, user, login, logout }}>{children}</AppwriteContext.Provider>;
 }
