f0947e5e7db892f63c16cdb3f1026ec11f0e79ef
hserranome <hendy@hserrano.me>
2024-04-21 14:15:17 +0200

    top title gradient

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 import Colors from "@/constants/Colors";
+import { LinearGradient } from "expo-linear-gradient";
 
 const styles = StyleSheet.create({
 	container: {
@@ -13,13 +14,18 @@ const styles = StyleSheet.create({
 		position: "absolute",
 		alignSelf: "center",
 		top: 0,
-		paddingTop: 80,
 		alignItems: "center",
+	},
+	topGradient: {
+		width: "100%",
+		height: "100%",
+		position: "absolute",
 	},
 	title: {
 		fontSize: 64,
 		fontWeight: "bold",
 		color: Colors.text,
+		marginTop: 80,
 	},
 	content: {
 		flex: 1,
@@ -36,6 +42,7 @@ export default function AuthLayout() {
 				<Slot />
 			</View>
 			<View style={styles.topContainer}>
+				<LinearGradient colors={["rgba(0,0,0,1)", "rgba(0,0,0,0.8)", "transparent"]} style={styles.topGradient} />
 				<Text style={styles.title}>Drawaday</Text>
 			</View>
 		</View>
